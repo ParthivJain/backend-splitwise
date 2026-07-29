0338e90dad9f23ae9321e70de72d8d909566167b
ParthivJain <jainparthiv2@gmail.com>
2026-07-29 21:20:50 +0530

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/splitwise/service/EmailService.java
+++ src/main/java/com/splitwise/service/EmailService.java
@@ -1,6 +1,7 @@
 package com.splitwise.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -11,24 +12,23 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
+    @Value("${MAIL_USERNAME}")
+    private String fromEmail;
+
     public void sendOtpMail(String to, String otp){
-        try {
-            SimpleMailMessage message = new SimpleMailMessage();
-            message.setFrom("splitwise.teams@gmail.com");
-            message.setTo(to);
-            message.setSubject("\uD83D\uDD10 Password Reset OTP - SplitWise");
-            message.setText(
-                    "Hello,\n\n" +
-                            "You requested to reset your password.\n" +
-                            "Your OTP is : " + otp + "\n\n" +
-                            "This OTP is valid for 10 minutes.\n\n" +
-                            "If you didn't request this, please ignore this email.\n\n" +
-                            "Thanks,\nSplitWise Team"
-            );
-            mailSender.send(message);
-        } catch (Exception e){
-            e.printStackTrace();
-        }
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(fromEmail);
+        message.setTo(to);
+        message.setSubject("\uD83D\uDD10 Password Reset OTP - SplitWise");
+        message.setText(
+                "Hello,\n\n" +
+                        "You requested to reset your password.\n" +
+                        "Your OTP is : " + otp + "\n\n" +
+                        "This OTP is valid for 10 minutes.\n\n" +
+                        "If you didn't request this, please ignore this email.\n\n" +
+                        "Thanks,\nSplitWise Team"
+        );
+        mailSender.send(message);
     }
 
 }
