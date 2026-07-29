e3c276b81018d884a106fb4588f979c139aa57bd
ParthivJain <jainparthiv2@gmail.com>
2026-07-29 20:15:17 +0530

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/splitwise/service/EmailService.java
+++ src/main/java/com/splitwise/service/EmailService.java
@@ -12,19 +12,23 @@ public class EmailService {
     private JavaMailSender mailSender;
 
     public void sendOtpMail(String to, String otp){
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom("splitwise.teams@gmail.com");
-        message.setTo(to);
-        message.setSubject("\uD83D\uDD10 Password Reset OTP - SplitWise");
-        message.setText(
-                "Hello,\n\n"+
-                "You requested to reset your password.\n"+
-                "Your OTP is : "+ otp + "\n\n"+
-                "This OTP is valid for 10 minutes.\n\n" +
-                "If you didn't request this, please ignore this email.\n\n" +
-                "Thanks,\nSplitWise Team"
-        );
-        mailSender.send(message);
+        try {
+            SimpleMailMessage message = new SimpleMailMessage();
+            message.setFrom("splitwise.teams@gmail.com");
+            message.setTo(to);
+            message.setSubject("\uD83D\uDD10 Password Reset OTP - SplitWise");
+            message.setText(
+                    "Hello,\n\n" +
+                            "You requested to reset your password.\n" +
+                            "Your OTP is : " + otp + "\n\n" +
+                            "This OTP is valid for 10 minutes.\n\n" +
+                            "If you didn't request this, please ignore this email.\n\n" +
+                            "Thanks,\nSplitWise Team"
+            );
+            mailSender.send(message);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
 }
